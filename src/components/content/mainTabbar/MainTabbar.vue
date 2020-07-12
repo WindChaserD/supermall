32a38a94779a4f4749cd862d6acfddbc0f6ec1a4
WindChaserD <781201469@qq.com>
2020-07-12 16:56:51 +0800

    tabbar添加

--- src/components/content/mainTabbar/MainTabbar.vue
+++ src/components/content/mainTabbar/MainTabbar.vue
@@ -1,33 +1,34 @@
 <template>
     <tab-bar>
+      <!-- 在DOM中使用虚拟路径需要在路径前面添加~符号 -->
       <tab-bar-item path="/home" activeColor="red">
-        <img slot="item-icon" src="../../assets/img/tabbar/home.svg">
-        <img slot="item-icon-active" src="../../assets/img/tabbar/home_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/home.svg">
+        <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item path="/category" activeColor="pink">
-        <img slot="item-icon" src="../../assets/img/tabbar/category.svg" alt="">
-        <img slot="item-icon-active" src="../../assets/img/tabbar/category_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item path="/cart" activeColor="pink">
-        <img slot="item-icon" src="../../assets/img/tabbar/shopcart.svg" alt="">
-        <img slot="item-icon-active" src="../../assets/img/tabbar/shopcart_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item path="/profile" activeColor="red">
-        <img slot="item-icon" src="../../assets/img/tabbar/profile.svg" alt="">
-        <img slot="item-icon-active" src="../../assets/img/tabbar/profile_active.svg" alt="">
+        <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
 </template>
 <script>
-import TabBar from '../tabbar/Tabbar'
-import TabBarItem from '../tabbar/TabBarItem'
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
 
 export default {
-    name:'MainTabbar',
+    name:'MainTabBar',
     components:{
         TabBar,
         TabBarItem
