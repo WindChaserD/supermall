8d1aa5973ebc0f511e001fe30895d161052fa465
WindChaserD <781201469@qq.com>
2020-07-13 12:25:10 +0800

    home_request_finished

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,19 +5,26 @@
 </template>
 <script>
     import NavBar from 'components/common/navbar/NavBar'
-    //import {getHomeMultidata} from "network/home";
+    import {getHomeMultidata} from "network/home";
 
     export default {
         name:"Home",
         components:{
             NavBar
         },
-        // created(){
-        //     //1请求多个数据
-        //     // getHomeMultidata().then(res => {
-        //     //     console.log(res)
-        //     // })
-        // }
+        data(){
+            return{
+                banner:[],
+                recommends:[]
+            }
+        },
+        created(){
+            //1.请求多个数据
+            getHomeMultidata().then(res => {
+                this.banner = res.data.banner.list
+                this.recommends = res.data.recommend.list
+            })
+        }
     }
 </script>
 <style scoped>
