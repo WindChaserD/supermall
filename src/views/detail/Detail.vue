ff624d0c471042e1ee040ff458b3be513ae80a90
WindChaserD <781201469@qq.com>
2020-07-25 16:59:40 +0800

    detailgoodsinfo_finished

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,29 +1,43 @@
 <template>
     <div id="detail">
-        <detail-nav-bar/>
-        <detail-swiper :top-images="topImages"/>
-        <detail-base-info :goods="goods"/>
+        <detail-nav-bar class="detail-nav"/>
+        <scroll class="content" ref="scroll">
+            <detail-swiper :top-images="topImages"/>
+            <detail-base-info :goods="goods"/>
+            <detail-shop-info :shop="shop" />
+            <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
+        </scroll>
     </div>
 </template>
 <script>
     import DetailNavBar from './childComps/DetailNavBar'
     import DetailSwiper from './childComps/DetailSwiper'
     import DetailBaseInfo from './childComps/DetailBaseInfo'
+    import DetailShopInfo from './childComps/DetailShopInfo'
+    import DetailGoodsInfo from './childComps/DetailGoodsInfo'
 
-    import {getDetail, Goods} from 'network/detail'
+    import Scroll from 'components/common/scroll/Scroll'
+
+    import {getDetail, Goods, Shop} from 'network/detail'
 
     export default {
         name:'Detail',
         components:{
             DetailNavBar,
             DetailSwiper,
-            DetailBaseInfo
+            DetailBaseInfo,
+            DetailShopInfo,
+            DetailGoodsInfo,
+            Scroll,
+            
         },
         data(){
             return{
                 iid:null,
                 topImages:[],
-                goods:null,
+                goods:{},
+                shop:{},
+                detailInfo:{},
             }
         },
         created(){
@@ -38,13 +52,36 @@
 
                 //2.获取商品信息
                 this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+                //3.创建店铺信息
+                this.shop = new Shop(data.shopInfo)
+
+                //4.保存商品的详情数据
+                this.detailInfo = data.detailInfo
             })
         },
-        mounted(){
-            
+        methods:{
+            imageLoad(){
+                this.$refs.scroll.refresh()
+            }
         }
     }
 </script>
 <style scoped>
+    #detail{
+        position: relative;
+        z-index: 9;
+        background-color: #fff;
+        height: 100vh;
+    }
 
+    .detail-nav{
+        position: relative;
+        z-index: 9;
+        background-color: #fff;
+    }
+
+    .content{
+        height: calc(100% - 44px);
+    }
 </style>
